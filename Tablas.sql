84fce8258971002a2bac335a9cb217eaba059505
victor <abana343@hotmail.com>
2019-01-29 11:04:04 -0300

    cambios
    se agrega el archivo de tablas para mantener las DB igual.
    y se elimina el parametro id y se remplaza por rut.

---
+++ Tablas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (  
+  `rut` varchar(20) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `surname` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
