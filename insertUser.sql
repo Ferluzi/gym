c9059d6b5914128eac92ecde7c34376da6d003e3
victor <abana343@hotmail.com>
2019-01-29 11:39:04 -0300

    cambios DB

--- insertUser.sql
+++ insertUser.sql
@@ -1,8 +1,8 @@
-INSERT INTO `user` (`rut`, `name`, `surname`, `email`, `password`, `created_at`) VALUES
-('17256958', 'manu', 'blamanu', 'manubla@bla.com', '123', '2018-11-26 12:43:02'),
-('18374934', 'bastian', 'ayala', 'basty2@bla.com', '123', '2018-11-26 12:43:02'),
-('19374934', 'bastian', 'medina', 'jaja2@bla.com', '123', '2018-11-26 12:43:02'),
-('20934934', 'victor', 'mobaty', 'tymoba@bla.com', '123', '2018-11-26 12:43:02')
-('20934934', 'nepe', 'ayala', 'nepe@bla.com', '123', '2018-11-26 12:43:02'),
-('18336958', 'noko', 'bla', 'nicolas@bla.com', '123', '2018-11-26 12:43:02'),
-('17526590-2', 'nikolas', 'muÃ±oz', 'nikolasmp92@gmail.com', '1427', '2018-11-26 13:05:07')
+INSERT INTO `user` (`rut`, `dv`, `name`, `surname`, `email`, `password`, `created_at`) VALUES
+('17256958', '9', 'manu', 'blamanu', 'manubla@bla.com', '123', '2018-11-26 12:43:02'),
+('18374934', '8','bastian', 'ayala', 'basty2@bla.com', '123', '2018-11-26 12:43:02'),
+('19374934', 'k','bastian', 'medina', 'jaja2@bla.com', '123', '2018-11-26 12:43:02'),
+('20934934', '9','victor', 'mobaty', 'tymoba@bla.com', '123', '2018-11-26 12:43:02'),
+('20924934', '7','nepe', 'ayala', 'nepe@bla.com', '123', '2018-11-26 12:43:02'),
+('18336958', '4','noko', 'bla', 'nicolas@bla.com', '123', '2018-11-26 12:43:02'),
+('17526590', '2','nikolas', 'muÃ±oz', 'nikolasmp92@gmail.com', '1427', '2018-11-26 13:05:07')
